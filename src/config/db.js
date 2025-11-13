2ca16fb9926e21cbfe328c7c0e4caa282190a51f
Cong Duan <phamcongduannn@gmail.com>
2025-11-13 14:53:51 +0700

    Kết nối db vs Prisma

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+// Create the connection to database
+const getConnection = async () => {
+    const connection = await mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "Duan0339514495@",
+        database: "db_ecommerce",
+    });
+};
+
+export default getConnection;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import getConnection from './config/db.js';
+import fileUpload from 'express-fileupload';
+import { json, urlencoded } from 'express';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8081;
@@ -9,11 +12,12 @@ app.use(fileUpload());
 // Config req.body
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
+//config static files
+app.use(express.static("public"));
 app.get('/', () => {
     return "Hello World"
 });
-
+getConnection()
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
